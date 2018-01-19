9bea33be2669106d6c43db504f2c3f3361ba63c8
alexdaffara <alexdaffara@128.54.209.106>
2018-01-18 22:43:32 -0800

    Added circle

--- MidiWheel/src/WheelGUI.java
+++ MidiWheel/src/WheelGUI.java
@@ -4,7 +4,9 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.scene.shape.Circle;
 
 public class WheelGUI extends Application{
 
@@ -13,9 +15,25 @@ public class WheelGUI extends Application{
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("MidiWheel");
+	public void start(Stage stage0) throws Exception {
+		stage0.setTitle("MidiWheel");
+		StackPane root = new StackPane();
 		
+		Circle outerCircle = new Circle(80);
+		Circle innerCircle = new Circle(77);
+		
+		innerCircle.setFill(Color.WHITE);
+		
+		root.getChildren().add(outerCircle);
+		root.getChildren().add(innerCircle);
+		
+		outerCircle.setCenterX(root.getWidth()/2);
+		outerCircle.setCenterY(root.getHeight()/2);
+		innerCircle.setCenterX(root.getWidth()/2);
+		innerCircle.setCenterY(root.getHeight()/2);
+		
+		stage0.setScene(new Scene(root, 200, 200));
+		stage0.show();
 	}
 
 }
