b399dd8ffe3ff2269e992bec61d9317759a61170
alexanderdaffara <35358386+alexanderdaffara@users.noreply.github.com>
2018-01-18 22:12:59 -0800

    first upload

---
+++ WheelGUI.java
@@ -0,0 +1,19 @@
+
+
+public class WheelGUI{
+
+	/**
+	 * 
+	 */
+	public WheelGUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//new Acme.MainFrame(new WheelGUI(), args, 500, 500);
+	}
+
+}
